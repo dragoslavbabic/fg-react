4fcb366a8cb2aec83b04b24ed9fe836564ba1c17
dragance <dragoslavbabic1971@gmail.com>
2020-05-28 11:46:31 +0200

    main page

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+  render () {
+    return (
+      <div>
+        <h1>
+          Simple SPA
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
